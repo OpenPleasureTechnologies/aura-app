e9f317d9e3b45f7f551c2a5dfa6d009cda532856
OpenPleasureTechnologies <dubailuxurydreamhomes@gmail.com>
2026-06-07 08:52:16 +0530

    Color.kt

---
+++ android/app/src/main/java/org/opt/aura/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package org.opt.aura.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AuraRose = Color(0xFFE8968A)
+val AuraRoseLight = Color(0xFFFFB4A8)
+val AuraRoseDark = Color(0xFFC97A6E)
+
+val AuraLavender = Color(0xFFA394E8)
+val AuraTeal = Color(0xFF6FD4AA)
+val AuraAmber = Color(0xFFE8C47A)
+
+val AuraBg = Color(0xFF06060E)
+val AuraSurface = Color(0xFF0C0C1A)
+val AuraText = Color(0xFFEDE8E2)
+val AuraMuted = Color(0xFF696375)
